4894632d017be4f23dc1c26237376905c8592743
Matthew McDonald <matthewmcdonald1963@gmail.com>
2019-06-16 09:43:56 -0600

    stupid stupid stupid error by the SYSTEM.  It has a case issue with apiroutes.

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -2,7 +2,7 @@
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on potential friends.
-// ===============================================================================
+// ================================================================================
 
 var friendData = require("../data/friends");
 
--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(express.json());
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// =================================================================================
 
-require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiroutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
